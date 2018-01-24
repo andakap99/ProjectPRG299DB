1ed28669d8e06b6ffa6578c455e46ca77bc8cfb2
andak <andak@LAPTOP-V0O9FE8O>
2018-01-23 20:40:47 -0500

    crypto class add

---
+++ ProjectPRG299BLL/Cryptography.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace ProjectPRG299BLL
+{
+    public class Cryptography
+    {
+        public class Generic
+        {
+
+        }
+    }
+}
